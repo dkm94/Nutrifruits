9e61a70adfd6f85a1e26fe7f71689e1cf1000002
Mandel <a.mandel06@gmail.com>
2020-07-21 15:48:47 +0200

    m base 2

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require('express');
-
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(__dirname));
 
 
+io.on('connection', () =>{
+  console.log('a user is connected');
+ })
 
 
 
