77ddc4383589cfbfaec054cc37d6a664ccfb41e6
Pierre-Alain-agn <pierrealainagn@gmail.com>
2020-07-21 16:27:39 +0200

    connect to database mongo atlas

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var express = require("express");
+var mongoose = require("mongoose");
+
 app.use(express.static(__dirname));
+
+const uri =
+  "mongodb+srv://Admin:MndQDlXM2OZqdtBt@cluster0.phlbn.mongodb.net/e_commerce?retryWrites=true&w=majority";
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 var server = app.listen(3010, () => {
   console.log("server is running on port", server.address().port);
